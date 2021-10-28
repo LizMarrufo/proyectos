57949782128a308b160699c9989b062e38ffcc5d
LizMarrufo <lizeth.marrufo97@gmail.com>
2021-10-28 12:10:42 -0600

    updated

---
+++ ModeloB/ModeloB.php
@@ -0,0 +1,12 @@
+<?php
+printf("Abriendo Archivo");
+$archivo_url = fopen("el_quijote.txt" , "r");
+
+$texto = "";
+
+while ($fragmento = fgets($archivo_url)) {
+    $texto .=$fragmento;
+}
+
+printf($texto);
+printf("<br>Haciendo otra cosa");
