56ee35efb1584ed0d7abf3dae470a9be8f47995a
LizMarrufo <lizeth.marrufo97@gmail.com>
2021-11-03 10:59:18 -0700

    updated

--- HTTP/HolaMundo.js
+++ HTTP/HolaMundo.js
@@ -9,9 +9,9 @@ const server = http.createServer((req, res) =>{
     //el servidor respondera un codigo 200
     res.statusCode = 200;
     //el servidor respondera con un texto plano
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     //el servidor respondera el mensaje hola mundo
-    res.end('Hola Mundo\n');
+    res.end('<h1>Hola Mundo</h1>');
 });
 
 server.listen(port, hostname, () =>{
